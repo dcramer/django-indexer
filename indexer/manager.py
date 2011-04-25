85b4c4784578d41197bf4a76dbefed5e67ff91d8
David Cramer <dcramer@gmail.com>
2011-04-25 11:11:09 -0700

    Create a BaseIndex which can be abstracted and used without having a generic indexer in your INSTALLED_APPS

--- indexer/manager.py
+++ indexer/manager.py
@@ -1,10 +1,7 @@
 from django.db.models import signals
 from django.db.models.manager import Manager
-from django.core.cache import cache
 
 from indexer.utils import Proxy
-
-import uuid
 
 COLUMN_SEPARATOR = '__'
 
@@ -28,7 +25,6 @@ class LazyIndexLookup(Proxy):
         
         app_label = model_class._meta.app_label
         module_name = model_class._meta.module_name
-        cache_key_base = ':'.join([app_label, module_name])
 
         if qs is None:
             qs = model_class.objects.all()
@@ -52,6 +48,43 @@ class LazyIndexLookup(Proxy):
                 params=[
                     module_name,
                     app_label,
+                    column,
+                    value,
+                ],
+            )
+
+        object.__setattr__(self, '__instance__', qs)
+        return qs
+    _current_object = property(_get_current_object)
+
+class BaseLazyIndexLookup(Proxy):
+    def _get_current_object(self):
+        """
+        Return the current object.  This is useful if you want the real object
+        behind the proxy at a time for performance reasons or because you want
+        to pass the object into a different context.
+        """
+        inst = self.__instance__
+        if inst is not None:
+            return inst
+        model, model_class, qs, pairs = self.__data__
+        
+        if qs is None:
+            qs = model_class.objects.all()
+
+        tbl = model._meta.db_table
+        main = model_class._meta.db_table
+        pk = model_class._meta.pk.column
+        for column, value in pairs.iteritems():
+            cid = tbl
+            qs = qs.extra(
+                tables=[tbl],
+                where=['%(cid)s.column = %%s and %(cid)s.value = %%s and %(cid)s.object_id = %(main)s.%(pk)s' % dict(
+                    cid=cid,
+                    pk=pk,
+                    main=main,
+                )],
+                params=[
                     column,
                     value,
                 ],
@@ -165,3 +198,95 @@ class IndexManager(Manager):
                 continue
             self.create(app_label=app_label, module_name=module_name, object_id=object_id, column=column, value=value)
         self.register_model(model_class, column)
+
+class BaseIndexManager(Manager):
+    def get_for_index(self, **kwargs):
+        if len(kwargs) < 1:
+            raise ValueError
+        
+        return LazyIndexLookup(self.model, self.model.get_model(), None, **kwargs)
+    
+    def get_for_queryset(self, queryset, **kwargs):
+        if len(kwargs) < 1:
+            raise ValueError
+        
+        return LazyIndexLookup(self.model, queryset.model, queryset, **kwargs)
+    
+    def register_index(self, column, index_to=None):
+        """Registers a model and an index for it."""
+        self.model.__indexes.add((column, index_to))
+        model_class = self.model.get_model()
+        signals.post_save.connect(self.model.handle_save, sender=model_class)
+        signals.pre_delete.connect(self.model.handle_delete, sender=model_class)
+    
+    def remove_from_index(self, instance):
+        tbl = self.model._meta.db_table
+        self.filter(object_id=instance.pk).delete()
+    
+    def save_in_index(self, instance, column, index_to=None):
+        """Updates an index for an instance.
+        
+        You may pass column as base__sub to access
+        values stored deeper in the hierarchy."""
+        if index_to:
+            index_to = instance._meta.get_field_by_name(index_to)[0]
+
+        if not index_to:
+            object_id = instance.pk
+        else:
+            object_id = getattr(instance, index_to.column)
+
+        value = instance
+        first = True
+        for bit in column.split(COLUMN_SEPARATOR):
+            if first:
+                value = getattr(value, bit)
+                first = False
+            elif value is not None:
+                value = value.get(bit)
+        if not value:
+            self.filter(object_id=object_id, column=column).delete()
+        else:
+            # TODO: in mysql this can be a single operation
+            qs = self.filter(object_id=object_id, column=column)
+            if qs.exists():
+                qs.update(value=value)
+            else:
+                self.create(object_id=object_id, column=column, value=value)
+
+    def create_index(self, column, index_to=None):
+        """Creates and prepopulates an index.
+        
+        You may pass column as base__sub to access
+        values stored deeper in the hierarchy."""
+        
+        # make sure the index exists
+        model_class = self.model.get_model()
+        
+        if index_to:
+            index_to = model_class._meta.get_field_by_name(index_to)[0]
+
+        # must pull from original data
+        qs = model_class.objects.all()
+        column_bits = column.split(COLUMN_SEPARATOR)
+
+        for m in qs:
+            if not index_to:
+                object_id = m.pk
+            else:
+                object_id = getattr(m, index_to.column)
+
+            value = m
+            first = True
+            for bit in column.split(COLUMN_SEPARATOR):
+                if first:
+                    value = getattr(value, bit)
+                    first = False
+                else:
+                    value = value.get(bit)
+            for bit in column_bits:
+                value = value.get(bit)
+            if not value:
+                continue
+            self.create(object_id=object_id, column=column, value=value)
+        self.register_index(column)
--- indexer/models.py
+++ indexer/models.py
@@ -1,8 +1,40 @@
 from django.db import models
 
-from indexer.manager import *
+from indexer.manager import IndexManager, BaseIndexManager
 
-__all__ = ('Index',)
+__all__ = ('BaseIndex', 'Index')
+
+class BaseIndex(models.Model):
+    object_id   = models.PositiveIntegerField()
+    column      = models.CharField(max_length=32)
+    value       = models.CharField(max_length=128)
+    
+    objects     = BaseIndexManager()
+    
+    __indexes     = set()
+    model       = None
+    
+    class Meta:
+        abstract = True
+        unique_together = (('column', 'value', 'object_id'),)
+
+    def __unicode__(self):
+        return "%s=%s where pk is %s" % (self.column, self.value, self.object_id)
+
+    @classmethod
+    def get_model(cls):
+        return cls.model
+
+    @classmethod
+    def handle_save(cls, sender, instance, created, **kwargs):
+        """Handles updating this model's indexes."""
+        for column, index_to in cls.__indexes:
+            cls.objects.save_in_index(instance, column, index_to)
+
+    @classmethod
+    def handle_delete(cls, sender, instance, **kwargs):
+        """Handles updating this model's indexes."""
+        cls.objects.remove_from_index(instance)
 
 class Index(models.Model):
     app_label   = models.CharField(max_length=32)
